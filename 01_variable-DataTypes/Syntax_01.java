5bac47a1d5267b8e61f7473b0a23e9a53205b1cc
Tushar Talwekar <tushartalwekar08@gmail.com>
2025-01-04 05:03:17 +0530

    add

--- 01_variable-DataTypes/Syntax_01.java
+++ 01_variable-DataTypes/Syntax_01.java
@@ -1,9 +1,21 @@
 // initialization and declaration of variable -
-public class Syntax_01{
-    public static void main(String[] args){
+public class Syntax_01{   // public class 
+    public static void main(String[] args){  // main function
         int a = 10;
         int b = 20;
         int sum = a + b;
         System.out.println(sum);
+
+        String name = "Tushar"; 
+        System.out.println(name);
+
+        int c; //declaration variable of int dataType
+        c = 30;  // initialize the value to variable c 
+        System.out.println(c); // print c
+
+        int d = 1;
+        d = 2; // update value
+        System.out.println("updated value :" + d); // 2
+
     }
 }
